2a576407a865da33bf72826ecd1e8a825aa5163b
divyansh <dahiya.divyansh1@gmail.com>
2021-05-08 02:22:18 +0530

    0.1(html done )

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("hjelrnfonoren");
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+todoButton.addEventListener("click", addTodo);
+
+function addTodo(event) {
+  event.preventDefault();
+  console.log("wepnfonweonf");
+}
